50f46b0d5c3116cff5834a87f6f72d609592a116
karokojnr <karoko.jnr99@gmail.com>
2021-02-03 14:29:34 +0300

    1st commit:pushpin:

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>karokojnr Blog</h1>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
+                    color: 'white',
+                    backgroundColor: '#008080',
+                    borderRadius: '8px'
+                }}>New Blog</Link>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
